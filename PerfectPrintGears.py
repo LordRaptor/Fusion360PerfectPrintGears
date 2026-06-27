2d94a77f507d3c849a254069e7e9f30d52724ec5
Christopher Schank <isc_raptor@yahoo.de>
2026-06-27 20:37:57 +0200

    fix(fusion): suppress startup message box when auto-loading at launch

--- PerfectPrintGears.py
+++ PerfectPrintGears.py
@@ -9,13 +9,17 @@ def run(context):
         futil.log('Perfect Print Gears: run() -- loading add-in')
         commands.start()
         futil.log('Perfect Print Gears: loaded, log file at ' + futil.LOG_FILE)
-        app = adsk.core.Application.get()
-        app.userInterface.messageBox(
-            'The Perfect Print Gears add-in has loaded and added a '
-            '"Generate Perfect Print Gears" command to the CREATE panel in the '
-            'SOLID tab of the DESIGN workspace.\n\n'
-            'Debug log: ' + futil.LOG_FILE,
-            'Perfect Print Gears')
+
+        # Display a message when the add-in is manually run.
+        if not context['IsApplicationStartup']:
+            app = adsk.core.Application.get()
+            app.userInterface.messageBox(
+                'The Perfect Print Gears add-in has loaded and added a '
+                '"Generate Perfect Print Gears" command to the CREATE panel in the '
+                'SOLID tab of the DESIGN workspace.\n\n'
+                'Debug log: ' + futil.LOG_FILE,
+                'Perfect Print Gears')
+
     except Exception:
         futil.handle_error('run', show_message_box=True)
 
